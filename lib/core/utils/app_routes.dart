5a37475dc4c57eec26a5b111cfc672d06e0fc387
Marriiamm <marriiamm453@gmail.com>
2024-01-21 21:01:19 +0200

    create search view

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lit_library/features/search/ui/views/search_view.dart';
 
 import '../../features/home/ui/views/book_details_view.dart';
 import '../../features/splash/ui/views/splash_view.dart';
@@ -6,6 +7,7 @@ import '../../features/splash/ui/views/splash_view.dart';
 class AppRoutes {
   static const String splash = "/";
   static const String bookDetails = "/BookDetailsView";
+  static const String search = "/SearchView";
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -13,7 +15,8 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => const SplashView());
       case bookDetails:
         return MaterialPageRoute(builder: (_) => const BookDetailsView());
-
+      case search:
+        return MaterialPageRoute(builder: (_) => const SearchView());
       default:
         return MaterialPageRoute(
             builder: (_) => Scaffold(
